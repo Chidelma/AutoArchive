437e4c17e6460799da0a7d10f05b4477bfce864b
Chinedu Ezenma <lycosidae23@gmail.com>
2019-08-10 13:21:39 -0500

    INIT REPO

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AUTO_ARCHIVE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
